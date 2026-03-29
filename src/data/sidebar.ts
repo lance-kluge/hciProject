c509e0615d87a9d6b5ecae0371dbe5d5b53bd1ce
Lance Kluge <lance.kluge@gmail.com>
2026-03-29 17:04:15 -0600

    init left sidebar

---
+++ src/data/sidebar.ts
@@ -0,0 +1,17 @@
+export const unreadCount = 12;
+export const className = 'CSCI 3002: Human-Computer Interaction';
+
+export const inboxFilters = [
+	'Unanswered',
+	'Needs Instructor',
+	'Instructor Endorsed',
+	'My Posts'
+];
+
+export const threadFilters = ['Homeworks', 'Exams', 'Projects', 'General'];
+
+export const messagingFilters = [
+	'Sent Messages',
+	'Pending Responses',
+	'Important Messages'
+];
