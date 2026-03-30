3d293919a980899bb71f6d070a4c0ee795d5ad25
Lance Kluge <lance.kluge@gmail.com>
2026-03-29 21:04:03 -0600

    class selector drop down added

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -1,5 +1,10 @@
 export const unreadCount = 12;
-export const className = 'CSCI 3002: Human-Computer Interaction';
+export const selectedClass = 'CSCI 3002: Human-Computer Interaction';
+export const classOptions = [
+	'CSCI 3002: Human-Computer Interaction',
+	'Test Class 1',
+	'Test Class 2'
+];
 
 export const inboxFilters = [
 	'Unanswered',
