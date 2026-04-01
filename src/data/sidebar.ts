04ea7b400b9168bfa5afaa83b9a32e60e0fb2843
kyndranelson <kyndra.nelson@colorado.edu>
2026-04-01 14:25:50 -0600

    done with messaging capability

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -1,15 +1,15 @@
 export const unreadCount = 12;
 export const selectedClass = 'CSCI 3002: Human-Computer Interaction';
 export const classOptions = [
-	'CSCI 3002: Human-Computer Interaction',
-	'Test Class 1',
-	'Test Class 2'
+  'CSCI 3002: Human-Computer Interaction',
+  'Test Class 1',
+  'Test Class 2'
 ];
 
 export const threadFilters = ['Homeworks', 'Exams', 'Projects', 'General'];
 
 export const messagingFilters = [
-	'Sent Messages',
-	'Pending Responses',
-	'Important Messages'
+  'Sent Messages',
+  'Pending Responses',
+  'Important Messages'
 ];
